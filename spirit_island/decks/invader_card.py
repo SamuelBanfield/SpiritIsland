a3b89edcd3a32bfaeae2c099ed25923553729fff
SamuelBanfield <55929837+SamuelBanfield@users.noreply.github.com>
2023-10-17 23:27:29 +0100

    Add a hash to invader card

--- spirit_island/decks/invader_card.py
+++ spirit_island/decks/invader_card.py
@@ -13,3 +13,9 @@ class InvaderCard(Card):
     def __eq__(self, other):
         """See if a card is equivalent for testing"""
         return self.terrains == other.terrains and self.stage == other.stage
+
+    def __hash__(self):
+        """Hash so that this can be used in dictionaries"""
+        terrain_hash = hash(tuple(self.terrains))
+        stage_hash = hash(self.stage)
+        return terrain_hash ^ stage_hash
