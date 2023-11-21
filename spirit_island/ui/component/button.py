0e111317f5d0d28f3753c2273823dd322a275706
SamuelBanfield <55929837+SamuelBanfield@users.noreply.github.com>
2023-11-21 21:21:43 +0000

    Allow buttons to say when they're enabled

--- spirit_island/ui/component/button.py
+++ spirit_island/ui/component/button.py
@@ -9,19 +9,22 @@ from spirit_island.ui.util import BLACK, FONT_SUPPLIER, GREY, WHITE
 
 class TextButton(UIComponent):
     def __init__(
-        self, text_or_provider: str | Callable, callback: Callable = None, offset=[0, 0], font_size: int = 24
+        self, text_or_provider: str | Callable, callback: Callable = None, offset=[0, 0], enablement = lambda: True, font_size: int = 24
     ):
         """
         :param text_or_provider: either the text if this button always has
         the same text, or a callback returning the button text
         :param callback: the callback called when the button is pressed
         :param offset: top left of the button on its parent surface
+        :param enablement: when this button should detect clicks (so the UI
+        can display that the button won't do anything). Defaults to always
         :param font_size: size of the font
         """
         super().__init__()
         self._callback = callback
         self._font = FONT_SUPPLIER.get_font_size(font_size)
         self._offset = offset
+        self._is_enabled = enablement
         if isinstance(text_or_provider, str):
             self.get_text = lambda: text_or_provider
         else:
@@ -39,12 +42,12 @@ class TextButton(UIComponent):
     def render(self, dest: pygame.surface.Surface, hovered: bool):
         self.update_text()
         dest.blit(
-            self._font_surface_hover if hovered else self._font_surface, self._font_rect
+            self._font_surface_hover if hovered and self._is_enabled() else self._font_surface, self._font_rect
         )
 
     @override
     def handle_click(self, click_location):
-        if self._callback:
+        if self._callback and self._is_enabled():
             self._callback()
 
     @override
